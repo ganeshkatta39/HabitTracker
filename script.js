1048fbb01dbae91c30768cae1860750c9e0f3a6b
Ganesh <67380725+ganeshkatta39@users.noreply.github.com>
2023-10-05 06:29:17 +0530

    added the db functionality

--- script.js
+++ script.js
@@ -4,14 +4,40 @@ let neg_buttons = document.getElementsByClassName("-button");
 
 for (let i = 0; i < pos_buttons.length; i++) {
   pos_buttons[i].addEventListener("dblclick", (ele) => {
-    points.innerHTML =
-      parseInt(points.innerHTML) + parseInt(pos_buttons[i].value);
+    update(ref(database, "points/" + "-NfxCecS27A_diIPkrRd"), {
+      value: parseInt(points.innerHTML) + parseInt(pos_buttons[i].value),
+    });
   });
 }
 
 for (let i = 0; i < neg_buttons.length; i++) {
-  neg_buttons[i].addEventListener("click", (ele) => [
-    (points.innerHTML =
-      parseInt(points.innerHTML) - parseInt(neg_buttons[i].value)),
-  ]);
+  neg_buttons[i].addEventListener("click", (ele) => {
+    update(ref(database, "points/" + "-NfxCecS27A_diIPkrRd"), {
+      value: parseInt(points.innerHTML) - parseInt(neg_buttons[i].value),
+    });
+  });
 }
+
+// db
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+  update,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const firebaseConfig = {
+  databaseURL: "https://habit-tracker-ad0d2-default-rtdb.firebaseio.com/",
+};
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const pointsInDb = ref(database, "points");
+
+// push(pointsInDb, 1000);
+onValue(pointsInDb, function (snapshot) {
+  let pointsarr = Object.values(snapshot.val());
+  points.innerHTML = pointsarr[0].value;
+});
