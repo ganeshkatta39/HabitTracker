d5b5c2dc54ee63012df3ceb3ea6a672cd4b96ecd
Ganesh <67380725+ganeshkatta39@users.noreply.github.com>
2023-10-05 19:55:21 +0530

    added tasks removability and restability

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 let points = document.getElementById("points");
-let pos_buttons = document.getElementsByClassName("button");
-let neg_buttons = document.getElementsByClassName("-button");
 
-for (let i = 0; i < pos_buttons.length; i++) {
-  pos_buttons[i].addEventListener("dblclick", (ele) => {
-    update(ref(database, "points/" + "-NfxCecS27A_diIPkrRd"), {
-      value: parseInt(points.innerHTML) + parseInt(pos_buttons[i].value),
-    });
-  });
+// adding tasks
+let tasksList = document.getElementById("taskList");
+function addTask(taskArr) {
+  for (let i = 0; i < taskArr.length; i++) {
+    if (taskArr[i][1].status == false) {
+      let taskLi = document.createElement("li");
+      taskLi.innerHTML = `<b>+${taskArr[i][1].reward}</b> &nbsp ${taskArr[i][1].name}`;
+      taskLi.addEventListener("click", (ele) => {
+        update(ref(database, "points/" + "-NfxCecS27A_diIPkrRd"), {
+          value: parseInt(points.innerHTML) + parseInt(taskArr[i][1].reward),
+        });
+        update(ref(database, "tasks/" + taskArr[i][0]), {
+          status: true,
+        });
+      });
+      tasksList.append(taskLi);
+    }
+  }
 }
 
-for (let i = 0; i < neg_buttons.length; i++) {
-  neg_buttons[i].addEventListener("click", (ele) => {
-    update(ref(database, "points/" + "-NfxCecS27A_diIPkrRd"), {
-      value: parseInt(points.innerHTML) - parseInt(neg_buttons[i].value),
-    });
+// resetting tasks
+let resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", () => {
+  onValue(tasksInDb, function (snapshot) {
+    let tasksArr = Object.entries(snapshot.val());
+    for (let i = 0; i < tasksArr.length; i++) {
+      update(ref(database, "tasks/" + tasksArr[i][0]), {
+        status: false,
+      });
+    }
   });
-}
+});
 
 // db
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
@@ -35,8 +50,64 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const pointsInDb = ref(database, "points");
+const tasksInDb = ref(database, "tasks");
+
+// push(tasksInDb, {
+//   name: "Early start (up at 4, brush)",
+//   reward: 25,
+//   status: false,
+// });
+// push(tasksInDb, {
+//   name: "Take care of body (exercise, jog,bath)",
+//   reward: 50,
+//   status: false,
+// });
+
+// push(tasksInDb, {
+//   name: "Take care of mind (meditate,self-affirmation, read)",
+//   reward: 15,
+//   status: false,
+// });
+
+// push(tasksInDb, {
+//   name: "Coding",
+//   reward: 50,
+//   status: false,
+// });
+
+// push(tasksInDb, {
+//   name: "Projects, papers, patents, research",
+//   reward: 30,
+//   status: false,
+// });
+
+// push(tasksInDb, {
+//   name: "Selfimprovement (write,learn,language, talk, act,etc)",
+//   reward: 20,
+//   status: false,
+// });
+
+// push(tasksInDb, {
+//   name: "End of day (days highlight,gratitude,journal)",
+//   reward: 10,
+//   status: false,
+// });
+
+// push(tasksInDb, {
+//   name: "Youtube video",
+//   reward: 50,
+//   status: false,
+// });
 
 // push(pointsInDb, 1000);
+
+onValue(tasksInDb, function (snapshot) {
+  let tasksArr = Object.entries(snapshot.val());
+  tasksList.innerHTML = "";
+  addTask(tasksArr);
+  console.log("change");
+});
+
 onValue(pointsInDb, function (snapshot) {
   let pointsarr = Object.values(snapshot.val());
   points.innerHTML = pointsarr[0].value;
